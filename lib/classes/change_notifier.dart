aa6f34b78cbafeb904a58a520f4863f2f247eab5
RemoCharles <remo.gemperli@hotmail.ch>
2025-09-28 09:28:29 +0200

    refactor: improve wine filtering logic and ensure state consistency

--- lib/classes/change_notifier.dart
+++ lib/classes/change_notifier.dart
@@ -3,72 +3,99 @@ import 'package:wiih/classes/wine/wine.dart';
 import 'package:wiih/classes/wine/wine_notes.dart';
 
 class WineList with ChangeNotifier {
-  bool _isFiltered = false;
-  List<Wine> get wines => _isFiltered ? _filteredWines : _wines;
   List<Wine> _wines = [];
   List<Wine> _filteredWines = [];
+  bool _isFiltered = false;
+  String? _activeFilterType;
 
+  List<Wine> get wines => _isFiltered ? _filteredWines : _wines;
 
   void addWine(Wine wine) {
     _wines.add(wine);
+    _refreshFilteredWines();
     notifyListeners();
   }
 
   void updateWine(Wine updatedWine) {
-    int index = _wines.indexWhere((wine) => wine.id == updatedWine.id);
+    final index = _wines.indexWhere((wine) => wine.id == updatedWine.id);
     if (index != -1) {
       _wines[index] = updatedWine;
+      _refreshFilteredWines();
       notifyListeners();
     }
   }
 
   void deleteWine(int wineId) {
+    final initialLength = _wines.length;
     _wines.removeWhere((wine) => wine.id == wineId);
-    notifyListeners();
+    if (_wines.length != initialLength) {
+      _refreshFilteredWines();
+      notifyListeners();
+    }
   }
 
   void sortWinesByPrice() {
     _wines.sort((a, b) => a.price.compareTo(b.price));
+    _refreshFilteredWines();
     notifyListeners();
   }
 
   void sortWinesByType() {
     _wines.sort((a, b) => a.type.compareTo(b.type));
+    _refreshFilteredWines();
     notifyListeners();
   }
 
   void sortWinesByCountry() {
     _wines.sort((a, b) => a.country.compareTo(b.country));
+    _refreshFilteredWines();
     notifyListeners();
   }
 
   void sortWinesByYear() {
     _wines.sort((a, b) => a.year.compareTo(b.year));
+    _refreshFilteredWines();
     notifyListeners();
   }
 
   void sortWinesByName() {
     _wines.sort((a, b) => a.name.compareTo(b.name));
+    _refreshFilteredWines();
     notifyListeners();
   }
 
   void loadWines(List<Wine> wines) {
-    _wines = wines;
+    _wines = List<Wine>.from(wines);
+    _refreshFilteredWines();
     notifyListeners();
   }
 
-  
   void filterWinesByType(String type) {
-    _filteredWines = _wines.where((wine) => wine.type == type).toList();
-    _isFiltered = true;
+    _activeFilterType = type;
+    _refreshFilteredWines();
     notifyListeners();
   }
 
   void clearFilter() {
-    _filteredWines.clear();
-    _isFiltered = false;
+    if (!_isFiltered && _activeFilterType == null) {
+      return;
+    }
+
+    _activeFilterType = null;
+    _refreshFilteredWines();
     notifyListeners();
-}
+  }
+
+  void _refreshFilteredWines() {
+    if (_activeFilterType == null) {
+      _filteredWines = [];
+      _isFiltered = false;
+      return;
+    }
+
+    _filteredWines = _wines.where((wine) => wine.type == _activeFilterType).toList();
+    _isFiltered = true;
+  }
 }
 
 class NotesList with ChangeNotifier {
